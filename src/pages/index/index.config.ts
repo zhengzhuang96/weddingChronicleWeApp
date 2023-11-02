d62900ea70895a41d0b52e9db6ca69705c10fce1
zhengzhuang <zhengzhuang@yuanduyun.com>
2023-11-02 22:18:14 +0800

    feat: 新增首页文案

--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,3 +1,3 @@
 export default definePageConfig({
-  navigationBarTitleText: '首页'
-})
+  navigationBarTitleText: "Mr.Zheng & Mrs.Yang",
+});
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,10 +2,10 @@
  * @Author: {zhengzhuang}
  * @Date: 2023-11-01 23:28:38
  * @LastEditors: {zhengzhuang}
- * @LastEditTime: 2023-11-01 23:39:33
+ * @LastEditTime: 2023-11-02 22:17:02
  * @Description:
  */
-import { View, Text } from "@tarojs/components";
+import { View, Text, Image, ScrollView } from "@tarojs/components";
 import { useLoad } from "@tarojs/taro";
 import "./index.less";
 
@@ -15,11 +15,28 @@ export default function Index() {
   });
 
   return (
-    <View className="index">
-      <Text>Hello world!</Text>
-      <Text>郑先生!</Text>
-      <Text>郑先生!</Text>
-      <Text>郑先生!</Text>
-    </View>
+    <ScrollView className="index">
+      <View className="index-banner">
+        <Text>这里放一张图片</Text>
+      </View>
+      <View className="index-banner">
+        <Text>这里放一张图片</Text>
+      </View>
+      <View className="index-text-box">
+        <Text className="head-title">亲爱的家人朋友，你好呀！</Text>
+        <Text className="head-des">收到这封邀请函的你们</Text>
+        <Text className="head-des">都是我们人生中最重要的部分</Text>
+        <Text className="head-des">在这特殊的一天</Text>
+        <Text className="head-des">希望有你的见证</Text>
+        <Text className="head-des">期待有你的参与</Text>
+      </View>
+      <View className="index-banner">
+        <Text>这里放一张图片</Text>
+      </View>
+      <View className="index-text-box">
+        <Text className="head-des">谢邀你和家人参加我们的婚礼</Text>
+        <Text className="head-des">见证我们的幸福</Text>
+      </View>
+    </ScrollView>
   );
 }
